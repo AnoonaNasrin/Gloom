424f82d5adceb80f4e4b5594388e41c5ab56391f
mohammed.basim <mohammed.basim@flytbase.com>
2022-12-19 00:17:56 +0530

    peerjs update

--- client/src/components/Chat/components/Video/Video.js
+++ client/src/components/Chat/components/Video/Video.js
@@ -77,7 +77,7 @@ export default function VideoScreen(props) {
       .then((stream) => {
         const peer = new Peer(undefined, {
           path: "/peerjs",
-          host: "/",
+          host: "https://www.gloomchat.ml/",
           port: "443",
         });
 
